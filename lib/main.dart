029df5854b3cd70142ca3313489ad98384b5a851
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2024-01-20 15:50:08 +0900

    init push and install package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'package:chat_flutter/firebase_options.dart';
 import 'package:chat_flutter/start_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
