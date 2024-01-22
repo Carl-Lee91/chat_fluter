a256a6bf1a432767c20105e47aa23b8d630b1f7d
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2024-01-22 12:11:35 +0900

    feat: add welcome screen

---
+++ lib/screens/onboarding/welcome_onbording_screen.dart
@@ -0,0 +1,50 @@
+import 'package:chat_flutter/const/functions.dart';
+import 'package:chat_flutter/screens/widgets/gradient.dart';
+import 'package:chat_flutter/screens/widgets/img_hero.dart';
+import 'package:chat_flutter/screens/widgets/text_btn.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class WelcomeOnboardingScreen extends StatelessWidget {
+  const WelcomeOnboardingScreen({super.key});
+
+  static const String routeName = "welcome";
+  static const String routeUrl = "/welcome";
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (didPop) => Functions.popScope(didPop, context),
+      child: Scaffold(
+        body: BackGroundGradient(
+          innerContext: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const ImgHero(),
+              Gap(MediaQuery.of(context).size.height * 0.1),
+              const Text(
+                "환영합니다!",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 36,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              Gap(MediaQuery.of(context).size.height * 0.1),
+              TextBtn(
+                width: 150,
+                height: 50,
+                text: "시작하기",
+                textSize: 18,
+                textColor: Colors.white,
+                onPressed: () {},
+                color: Theme.of(context).primaryColor,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
