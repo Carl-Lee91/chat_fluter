27fb1503fd07456c9b79ef85012595c806bf9791
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2024-01-22 12:10:03 +0900

    feat: add router

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,7 @@
-import 'package:chat_flutter/screens/auth/signup/signup_screen_else.dart';
+import 'package:chat_flutter/screens/auth/login/login_screen.dart';
+import 'package:chat_flutter/screens/auth/signup/signup_screen_auth.dart';
 import 'package:chat_flutter/screens/onboarding/start_onbording_screen.dart';
+import 'package:chat_flutter/screens/onboarding/welcome_onbording_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -17,6 +19,16 @@ final routerProvider = Provider(
           name: SignUpAuthScreen.routeName,
           path: SignUpAuthScreen.routeUrl,
           builder: (context, state) => const SignUpAuthScreen(),
+        ),
+        GoRoute(
+          name: LoginScreen.routeName,
+          path: LoginScreen.routeUrl,
+          builder: (context, state) => const LoginScreen(),
+        ),
+        GoRoute(
+          name: WelcomeOnboardingScreen.routeName,
+          path: WelcomeOnboardingScreen.routeUrl,
+          builder: (context, state) => const WelcomeOnboardingScreen(),
         ),
       ],
     );
